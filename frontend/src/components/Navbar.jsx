9105fd6275ea2c2fad6702ed4acd575046b1bd44
PavanBhaskarThota <pavanbhaskarthota05@gmail.com>
2024-01-12 00:51:33 +0530

    day4

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,23 +8,82 @@ import cart from "../Assets/navbar/ant-design_shopping-cart-outlined.svg";
 
 export const Navbar = () => {
   return (
-    <div style={{ display: "flex", justifyContent: "space-between", alignItems:'center', height:"100px" }}>
-      <div style={{ display: "flex", justifyContent: "space-between", alignItems:"center" , width:'12%' ,marginLeft:"56px"}}>
-        <div>
-          <img src={logo} alt="" />
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        height: "100px",
+      }}
+    >
+      <Link to={"/"} style={{ textDecoration: "none", color: "black" }}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            width: "12%",
+            marginLeft: "56px",
+            gap: "10px",
+          }}
+        >
+          <div>
+            <img src={logo} alt="" />
+          </div>
+
+          <h2
+            style={{
+              fontSize: "34px",
+              fontWeight: "700",
+              fontFamily: "Montserrat",
+            }}
+          >
+            Funiro
+          </h2>
         </div>
+      </Link>
 
-        <h2 style={{fontSize:"34px"}}>Funiro</h2>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          width: "28%",
+        }}
+      >
+        <Link
+          to={"/"}
+          style={{ fontSize: "16px", textDecoration: "none", color: "black" }}
+        >
+          Home
+        </Link>
+        <Link
+          to={"/shop"}
+          style={{ fontSize: "16px", textDecoration: "none", color: "black" }}
+        >
+          shop
+        </Link>
+        <Link
+          to={"/about"}
+          style={{ fontSize: "16px", textDecoration: "none", color: "black" }}
+        >
+          about
+        </Link>
+        <Link
+          to={"/contact"}
+          style={{ fontSize: "16px", textDecoration: "none", color: "black" }}
+        >
+          contact
+        </Link>
       </div>
 
-      <div style={{ display: "flex", justifyContent: "space-between" , width:'28%'}}>
-        <Link to={"/"} style={{fontSize:"16px" , textDecoration:"none", color:"black"}}>Home</Link>
-        <Link to={"/shop"} style={{fontSize:"16px" , textDecoration:"none", color:"black"}}>shop</Link>
-        <Link to={"/about"} style={{fontSize:"16px" , textDecoration:"none", color:"black"}}>about</Link>
-        <Link to={"/contact"} style={{fontSize:"16px" , textDecoration:"none", color:"black"}}>contact</Link>
-      </div>
-
-      <div style={{ display: "flex", justifyContent: "space-between" , width:'13%', marginRight:'100px'}}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          width: "13%",
+          marginRight: "100px",
+        }}
+      >
         <div>
           <img src={accAlert} alt="" />
         </div>
